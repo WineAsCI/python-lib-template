a758f7b68c7df5c53192af33c9c2524872909366
Felix Yan <felixonmars@gmail.com>
2015-02-06 16:47:03 +0800

    use 32-bit wine to make things easier

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y wine-staging winbind
 
 #wget http://www.orbitals.com/programs/py.exe
 wget https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi
-/opt/wine-staging/bin/wine msiexec /i python-2.7.9.msi
+WINEARCH=win32 /opt/wine-staging/bin/wine msiexec /i python-2.7.9.msi
 
 sed -i 's/_windows_cert_stores = .*/_windows_cert_stores = ("ROOT",)/' "$HOME/.wine/drive_c/Python27/Lib/ssl.py"
 
