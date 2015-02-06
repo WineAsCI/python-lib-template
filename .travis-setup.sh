f70af0272f2410572495b0cd9ca49b4c6c16931c
Felix Yan <felixonmars@gmail.com>
2015-02-06 13:40:40 +0800

    install winbind too

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository -y ppa:pipelight/daily
 sudo apt-get -qy update
-sudo apt-get install -y wine-staging
+sudo apt-get install -y wine-staging winbind
 wget http://www.orbitals.com/programs/py.exe
