10559aa3c677baed23c8c314025e1b9a8b0c1bec
Felix Yan <felixonmars@gmail.com>
2015-02-06 15:54:33 +0800

    fix the sed line

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y wine-staging winbind
 wget https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi
 /opt/wine-staging/bin/wine msiexec /i python-2.7.9.msi
 
-sed -i 's/_windows_cert_stores = .*/_windows_cert_stores = ("ROOT",)' "$HOME/.wine/drive_c/Python27/Lib/ssl.py"
+sed -i 's/_windows_cert_stores = .*/_windows_cert_stores = ("ROOT",)/' "$HOME/.wine/drive_c/Python27/Lib/ssl.py"
 
 wget https://bootstrap.pypa.io/ez_setup.py -O - | /opt/wine-staging/bin/wine 'C:\Python27\Python.exe'
 /opt/wine-staging/bin/wine 'C:\Python27\Scripts\easy_install.exe' pip
