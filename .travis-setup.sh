2a17234ae93821ccbc0d06b97d645ae799ee56c1
Felix Yan <felixonmars@gmail.com>
2015-02-06 15:52:09 +0800

    make the workaround permanentwq

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -6,7 +6,8 @@ sudo apt-get install -y wine-staging winbind
 wget https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi
 /opt/wine-staging/bin/wine msiexec /i python-2.7.9.msi
 
-wget https://bootstrap.pypa.io/ez_setup.py
-sed -i -e "37iimport ssl" -e '37issl.SSLContext._windows_cert_stores = ("ROOT",)' ez_setup.py
-/opt/wine-staging/bin/wine 'C:\Python27\Python.exe' ez_setup.py
+sed -i 's/_windows_cert_stores = .*/_windows_cert_stores = ("ROOT",)' "$HOME/.wine/drive_c/Python27/Lib/ssl.py"
+
+wget https://bootstrap.pypa.io/ez_setup.py -O - | /opt/wine-staging/bin/wine 'C:\Python27\Python.exe'
 /opt/wine-staging/bin/wine 'C:\Python27\Scripts\easy_install.exe' pip
+
