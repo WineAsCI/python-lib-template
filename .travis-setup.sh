998ac07f7fec0831c2fff927a0597927944834d7
Felix Yan <felixonmars@gmail.com>
2015-02-06 15:10:24 +0800

    try to use official python installer

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -1,4 +1,6 @@
 sudo add-apt-repository -y ppa:pipelight/daily
 sudo apt-get -qy update
 sudo apt-get install -y wine-staging winbind
-wget http://www.orbitals.com/programs/py.exe
+#wget http://www.orbitals.com/programs/py.exe
+wget https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi
+/opt/wine-staging/bin/wine msiexec /i python-2.7.9.msi
